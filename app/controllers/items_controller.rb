66a15cd4c7db206fc5d102ef53bdbc8e338df2ac
greatyo <yomiyoshiikeike@gmail.com>
2023-11-19 15:24:36 +0900

    買い物したものの表示を消す機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,7 @@ class ItemsController < ApplicationController
 
 
   def index
-    @items = Item.where(family_id: current_user.family_id)
-    #@items = Item.where(family_id: current_user.family_id,).not(user: nil)  itemが自分のfamilyかつuserが入っていて買い物登録されているを取り出せるか？
+    @items = Item.where(family_id: current_user.family_id).where.not(user_id: nil)
     @categories = Category.all
   end
 
@@ -20,6 +19,25 @@ class ItemsController < ApplicationController
     else
       render :index, status: :unprocessable_entity
     end
+  end
+
+  def update
+    # チェックボックスから送信されたアイテムのIDを取得
+    checked_item_ids = params[:item_ids]
+
+    # チェックされたアイテムがあればそれぞれのレコードを更新
+    if checked_item_ids.present?
+      checked_item_ids.each do |item_id|
+        item = Item.find_by(id: item_id, user_id: current_user.id)
+        # 特定のカラムを更新する例（user_idをnilに設定する）
+        item.update(user_id: nil) if item.present?
+      end
+    end
+
+    # リダイレクト先やフラッシュメッセージの設定など、適切な処理を行う
+
+    # 例えば、更新後にアイテム一覧ページにリダイレクトする場合
+    redirect_to index, notice: "選択されたアイテムが更新されました。"
   end
 
   private
