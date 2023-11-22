2f2fe7bca82ac398f4c4e58da039943b238b2f8c
greatyo <yomiyoshiikeike@gmail.com>
2023-11-23 04:48:35 +0900

    新規家族登録及び既存家族にidと家族名で加わる

--- app/controllers/familys_controller.rb
+++ app/controllers/familys_controller.rb
@@ -1,4 +1,20 @@
 class FamilysController < ApplicationController
-  def index
+  def new
+    @family = Family.new
+  end
+  
+  def create
+    @family = Family.new(family_params)
+    if @family.save
+      redirect_to new_user_registration_path(family_id: @family.id, name: @family.name)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def family_params
+    params.require(:family).permit(:name).merge(user_count: 0)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :familys do
+  resources :familys, only:[:new, :create] do
     resources :items, only:[:new, :create, :edit, :update, :destroy] do
       collection do
         get 'index_request'
